3df28842b6a2fe1fa8d7cd73d7c414d6d3aad7c7
JCMerlino <32750484+JCMerlino@users.noreply.github.com>
2025-11-16 14:00:09 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,8 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Create a Firebase project at https://console.firebase.google.com/
 // Register your app (Web) and paste the config here. Rename this file to firebase-config.js
 export const firebaseConfig = {
@@ -9,4 +14,10 @@ export const firebaseConfig = {
   messagingSenderId: "205801147540",
   appId: "1:205801147540:web:a20aef34c5981a894bf881"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 
