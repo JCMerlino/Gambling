fab596500221ff15b11e955fc292302be558248e
JCMerlino <32750484+JCMerlino@users.noreply.github.com>
2025-11-16 13:54:47 +0100

    Firebase Update

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Create a Firebase project at https://console.firebase.google.com/
+// Register your app (Web) and paste the config here. Rename this file to firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2DLlCW-X64oymcVcC42U_xFagHaIgb-U",
+  authDomain: "gambling-e298c.firebaseapp.com",
+  databaseURL: "https://gambling-e298c-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "gambling-e298c",
+  storageBucket: "gambling-e298c.firebasestorage.app",
+  messagingSenderId: "205801147540",
+  appId: "1:205801147540:web:a20aef34c5981a894bf881"
+};
+
